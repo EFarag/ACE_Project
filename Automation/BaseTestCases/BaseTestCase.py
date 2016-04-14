906b4f71898ebf3e305454c99c08bfe384565da9
EFarag <efarag@integrant.com>
2016-04-14 15:52:06 +0200

    Added files via upload

---
+++ Automation/BaseTestCases/BaseTestCase.py
@@ -0,0 +1,19 @@
+import unittest
+from selenium import webdriver
+
+
+class BaseTestCase(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://10.1.22.219/AGW")
+        self.driver.maximize_window()
+
+    @classmethod
+    def tearDown(self):
+        self.driver.close()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
