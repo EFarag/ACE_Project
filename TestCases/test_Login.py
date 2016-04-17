ef75ae1186df94af1b2bb68e93c099f18639b561
aabdelhamid <aabdelhamid@integrant.com>
2016-04-17 13:54:36 +0000

    Eman

--- TestCases/test_Login.py
+++ TestCases/test_Login.py
@@ -12,7 +12,7 @@ from DataSource.read_excel import read_excel
 class LoginTest(BaseTestCase):
 
 
-    @data(*read_excel.get_data_from_excel('C:/Users/Efarrag/Desktop/ACE_Automation001/ACE_Automation001/Data/login_data.xlsx','login'))
+    @data(*read_excel.get_data_from_excel('C:/Users/aabdelhamid/Desktop/ACE_Automation001/ACE_Automation001/Data/login_data.xlsx','login'))
     @unpack
     def test_login_with_valid_credentials(self,Username,Password,LoginName):
        LoginPage.login(self,Username,Password)
