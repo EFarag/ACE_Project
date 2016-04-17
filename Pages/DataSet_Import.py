fbf6c2ab79b78a3f98ba7f25aeddd94148330724
EFarrag <eman.f.abdelaziz@gmail.com>
2016-04-17 19:09:44 +0200

    Eman

--- Pages/DataSet_Import.py
+++ Pages/DataSet_Import.py
@@ -30,11 +30,19 @@ class Import_Password():
 
 class Upload():
  fileUpload =(By.XPATH,'/html/body/div[6]/div[2]/div[1]/input')
- SubmitButton = (By.XPATH,"/html/body/div[6]/div[3]/div/button[1]")
+ SubmitButton = (By.XPATH,'/html/body/div[6]/div[3]/div/button[1]')
 
  def File_Upload(self,location):
    self.driver.implicitly_wait(20)
-   self.driver.find_element(*Upload.fileUpload).send_keys('C:\VP Only 4DE7.mgr')
+   self.driver.find_element(*Upload.fileUpload).send_keys(location)
    self.driver.find_element(*Upload.SubmitButton).click()
 
 
+class ID_code():
+ ID_Code_TXT =(By.ID,'ctl00_ctl00_MasterPageContent_cpv_txtImportDSCode')
+ Import_Button = (By.XPATH,'/html/body/div[3]/div[3]/div/button[1]')
+
+ def Dataset_Import(self,IDC):
+   self.driver.find_element(*ID_code.ID_Code_TXT).send_keys(IDC)
+   self.driver.find_element(*ID_code.Import_Button).click()
+   self.driver.implicitly_wait(30)
--- TestCases/Import_DataSet.py
+++ TestCases/Import_DataSet.py
@@ -1,6 +1,6 @@
 import unittest
-
-#from ddt import ddt,data,unpack
+from DataSource.read_excel import read_excel
+from ddt import ddt,data,unpack
 from selenium import webdriver
 from Pages.HomePage import HomePage
 from Pages.LoginPage import LoginPage
@@ -9,22 +9,27 @@ from Pages.DataSet_Import import Dataset
 from Pages.DataSet_Import import Dataset_import
 from Pages.DataSet_Import import Import_Password
 from Pages.DataSet_Import import Upload
+from Pages.DataSet_Import import ID_code
 from time import sleep
 
 
 
-#@ddt
+@ddt
 class LoginTest(BaseTestCase):
 
 
-    def test_login_with_valid_credentials(self):
+   @data(*read_excel.get_data_from_excel('C:/Users/efarrag/PycharmProjects/ACE_Project/ACE_Project/Data/login_data.xlsx','Datasets'))
+   @unpack
+   def test_Upload(self,url,code):
        LoginPage.login(self,'Administrator','P@ssw0rd')
        sleep(1)
        Dataset.Dataset_link(self)
        Dataset_import.Import(self)
        Import_Password.Password(self,password='P@ssw0rd')
-       Upload.File_Upload(self,location='C:\VP Only 4DE7.mgr')
+       Upload.File_Upload(self,location=url)
+       ID_code.Dataset_Import(self,IDC=code)
        sleep(3)
+
 
 
 if __name__ == '__main__':
