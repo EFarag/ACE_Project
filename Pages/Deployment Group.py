c51e692e50b3130edc22b26ab553d10ade3457d7
engdgad <eng.dgad@gmail.com>
2016-05-09 18:23:41 +0200

    add ACE_project

--- Pages/Deployment Group.py
+++ Pages/Deployment Group.py
@@ -1,13 +1,21 @@
-
 import selenium
 import select
 from selenium.webdriver.common.by import By
 
 class DG_Create():
     CreateDG_btn = (By.ID,'ctl00_ctl00_MasterPageContent_cpv_lbCreate')
-
     def DG_create(self):
         self.driver.find_element (*DG_Create.DG_btn).click()
+
+class DG_edit():
+    Edit_btn=(By.XPATH,'/html/body/form/div[8]/section/section/div[1]/div[1]/a[2]')
+    def DG_edit(self):
+        self.driver.find_element (*DG_edit.Edit_btn).click
+
+class DG_delete():
+    Delete_btn=(By.XPATH,'/html/body/form/div[8]/section/section/div[1]/div[1]/a[3]')
+    def  DG_delete(self):
+        self.driver.find_element (*DG_delete.Delete_btn).click
 
 
 class DG_DetailsPopup():
@@ -17,7 +25,6 @@ class DG_DetailsPopup():
     DG_SaveConBtn=(By.XPATH,'/html/body/div[3]/div[3]/div/button[1]')
     DG_SaveCloseBtn=(By.XPATH,'/html/body/div[3]/div[3]/div/button[2]')
     DG_CancelBtn=(By.XPATH,'/html/body/div[3]/div[3]/div/button[3]')
-
     def DG_DetailsPopup(self):
         self.driver.find_element(*DG_DetailsPopup.DG_Name).send_keys('Group1')
         self.driver.find_element(*DG_DetailsPopup.DG_Description).send_keys('Group1')
