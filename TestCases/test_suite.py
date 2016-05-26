965bbf9b8de26ec2f7a81be6bf53d77a2ef2596c
EFarrag <eman.f.abdelaziz@gmail.com>
2016-05-26 19:44:22 +0200

    Eman

--- TestCases/test_suite.py
+++ TestCases/test_suite.py
@@ -1,6 +1,12 @@
 import unittest
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from TestCases.test_Login import LoginTest
 from Reporting_Lib import HTMLTestRunner
+import xmlrunner
 
 
 login_suite = unittest.TestLoader().loadTestsFromTestCase(LoginTest)
@@ -8,14 +14,10 @@ smoke_test = unittest.TestSuite([login_suite])
 
 outfile = open("D:\Report.html", "w")
 #create Test Report
-runner = HTMLTestRunner.HTMLTestRunner(
-            stream=outfile,
-            title= 'test',
-            description = 'smoke test suite report'
-        )
+runner = HTMLTestRunner.HTMLTestRunner(stream=outfile, title= 'test', description = 'smoke test suite report')
+
+# runner = xmlrunner.XMLTestRunner(output="test-results")
 runner.run(smoke_test)
-
-
 
 
 
