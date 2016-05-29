caaf602e7cbd04efa3cf3b489be8903e2936e590
EFarrag <eman.f.abdelaziz@gmail.com>
2016-05-29 11:36:45 +0200

    Eman

--- TestCases/test_suite.py
+++ TestCases/test_suite.py
@@ -1,10 +1,13 @@
 import os
-import sys
 import time
 import unittest
+import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\\TestCases")
+print (sys.path)
 
+import TestCases
 from TestCases.test_Login import LoginTest
 from TestCases.test_CreateUsers import test_users
 from TestCases.test_CreateRole import Roles
@@ -20,14 +23,10 @@ from TestCases.test_DG_AssignPumps import AssignPumps
 #from TestCases.test_DG_deletePmps import
 from TestCases.test_DG_AssignDS import Assign_DS
 from TestCases.test_DG_Delete import test_DGDelete
-from TestCases.test_Pumps import test_Pumps
+# from TestCases.test_Pumps import test_Pumps
 from TestCases.test_DGSave_cont import test_DG_Creation
-
-
-
-
 from Reporting_Lib import HTMLTestRunner
-import xmlrunner
+
 
 
 login_suite = unittest.TestLoader().loadTestsFromTestCase(LoginTest)
@@ -45,17 +44,16 @@ DeploymentGroup_Save_suite = unittest.TestLoader().loadTestsFromTestCase(test_DG
 DeploymentGroup_AssignDS_suite = unittest.TestLoader().loadTestsFromTestCase(Assign_DS)
 DeploymentGroup_AssignPumps_suite = unittest.TestLoader().loadTestsFromTestCase(AssignPumps)
 DeploymentGroup_Delete_suite = unittest.TestLoader().loadTestsFromTestCase(test_DGDelete)
-Pumps_suite = unittest.TestLoader().loadTestsFromTestCase(test_Pumps)
+# Pumps_suite = unittest.TestLoader().loadTestsFromTestCase(test_Pumps)
 
-smoke_test = unittest.TestSuite([login_suite,users_suite,Roles_Creation_suite,Role_Edit_suite,Role_Delete_suite,Datasets_Creation_suite,Datasets_Delete_suite,DeploymentGroup_ValidCreate_suite,DeploymentGroup_InvalidCreate_suite,DeploymentGroup_ValidEdit_suite,DeploymentGroup_InvalidEdit_suite,DeploymentGroup_Save_suite,DeploymentGroup_AssignDS_suite,DeploymentGroup_AssignPumps_suite,DeploymentGroup_Delete_suite,Pumps_suite ])
-#,Datasets_suite,DeploymentGroup_suite,Pumps_suite])
+smoke_test = unittest.TestSuite([login_suite,users_suite,Roles_Creation_suite,Role_Edit_suite,Role_Delete_suite,Datasets_Creation_suite,Datasets_Delete_suite,DeploymentGroup_ValidCreate_suite,DeploymentGroup_InvalidCreate_suite,DeploymentGroup_ValidEdit_suite,DeploymentGroup_InvalidEdit_suite,DeploymentGroup_Save_suite,DeploymentGroup_AssignDS_suite,DeploymentGroup_AssignPumps_suite,DeploymentGroup_Delete_suite])
 
-outfile = open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\Report" + time.strftime("%Y%m%d-%H%M%S") + ".html", "w")
+
+outfile = open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\Report_" + time.strftime("%Y%m%d-%H%M%S ") + ".html", "w")
 print (outfile.name)
 #create Test Report
-runner = HTMLTestRunner.HTMLTestRunner(stream=outfile, title= 'test', description = 'Smoke test report')
+runner = HTMLTestRunner.HTMLTestRunner(stream=outfile, title= 'ACE Execution Status', description = 'Smoke test report')
 print ("Runner prepared ..")
-# runner = xmlrunner.XMLTestRunner(output="test-results")
 runner.run(smoke_test)
 print ("Runner Done ..")
 
