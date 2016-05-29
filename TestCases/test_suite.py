48f3b6cb8c454a2a973b17f56a67f72de2ee6df1
EFarrag <eman.f.abdelaziz@gmail.com>
2016-05-29 11:25:29 +0200

    Eman

--- TestCases/test_suite.py
+++ TestCases/test_suite.py
@@ -4,6 +4,7 @@ import unittest
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print (sys.path)
 
 from TestCases.test_Login import LoginTest
 from TestCases.test_CreateUsers import test_users
